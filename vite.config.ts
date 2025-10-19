883fed2b6f441f2dffa84bfa3f2573451594faaf
M.N. Jeral Sandeeptha <jeral.sandeeptha1@gmail.com>
2025-10-19 20:20:57 +0530

    Added vitest unit testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -5,4 +6,12 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,            // allows `describe`, `it`, `expect` without imports
+    environment: 'jsdom',     // simulates browser environment
+    setupFiles: './src/setupTests.ts', // optional, for global setup
+    coverage: {
+      reporter: ['text', 'json', 'html'], // optional code coverage
+    },
+  },
 });
