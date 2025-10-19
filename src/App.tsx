0b960eb5ece753db62da9165d452c1c52ea91866
M.N. Jeral Sandeeptha <jeral.sandeeptha1@gmail.com>
2025-10-19 17:25:36 +0530

    Added Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 function App() {
-  return <>Nothing is true, and everything is permitted</>;
+  return (
+    <h1 className="text-3xl font-bold underline">Nothing is true, and everything is permitted</h1>
+  );
 }
 
 export default App;
