8e373ee63015e297f5b43141300e2cfc285092d2
M.N. Jeral Sandeeptha <jeral.sandeeptha1@gmail.com>
2025-10-19 17:14:03 +0530

    Added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 function App() {
-
-  return (
-    <>
-      Nothing is true, and everything is permitted
-    </>
-  )
+  return <>Nothing is true, and everything is permitted</>;
 }
 
 export default App;
