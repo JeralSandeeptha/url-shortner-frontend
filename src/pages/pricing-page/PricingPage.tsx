ff486fbdd79912c172cd1ec436100db0e1ba660d
M.N. Jeral Sandeeptha <jeral.sandeeptha1@gmail.com>
2025-10-31 06:17:39 +0530

    Added Pricing Page

--- src/pages/pricing-page/PricingPage.tsx
+++ src/pages/pricing-page/PricingPage.tsx
@@ -1,19 +1,19 @@
-import CtaSection from "../../components/cta-section/CtaSection";
-import Footer from "../../components/footer/Footer";
-import Header from "../../components/header/Header";
-import Hero from "../../components/hero/Hero";
-import Pricing from "../../components/pricing/Pricing";
+import CtaSection from '../../components/cta-section/CtaSection';
+import Footer from '../../components/footer/Footer';
+import Header from '../../components/header/Header';
+import Hero from '../../components/hero/Hero';
+import Pricing from '../../components/pricing/Pricing';
 
 const PricingPage = () => {
   return (
     <>
-        <Header />
-        <Hero />
-        <Pricing />
-        <CtaSection />
-        <Footer />
+      <Header />
+      <Hero />
+      <Pricing />
+      <CtaSection />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default PricingPage;
