e8d6fc591df74fd889137c90f36818dd00448b98
M.N. Jeral Sandeeptha <jeral.sandeeptha1@gmail.com>
2025-11-23 18:41:28 +0530

    Fixed prod env issues - 03

--- env.sh
+++ env.sh
@@ -9,17 +9,5 @@ export MODE="${MODE:-production}"
 # Explicitly list variables to substitute
 envsubst '$VITE_API_URL $MODE' < /usr/share/nginx/html/env.template.js > /usr/share/nginx/html/env.js
 
-# Inject env.js script tag into index.html before the main script
-# This ensures window._env_ is available before the app loads
-if ! grep -q '<script src="/env.js"></script>' /usr/share/nginx/html/index.html; then
-  # Use awk to inject the script tag (more reliable on Alpine)
-  awk '
-    /<script type="module" src="\/src\/main.tsx"><\/script>/ {
-      print "    <script src=\"/env.js\"></script>"
-    }
-    { print }
-  ' /usr/share/nginx/html/index.html > /tmp/index.html.new && mv /tmp/index.html.new /usr/share/nginx/html/index.html
-fi
-
 # Start Nginx
 exec nginx -g 'daemon off;'
