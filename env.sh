bf2317351e6cae5ddd58eb2bdd77114bbba1abe6
Jeral Sandeeptha <101165998+JeralSandeeptha@users.noreply.github.com>
2025-11-29 06:07:53 +0530

    Release/v1.0.0 (#10)
    * Added Login Page Structure
    
    * Almost complete Login Page Structure
    
    * Almost NotFound Page
    
    * Added Register Page
    
    * Added Reset Password Page
    
    * Added Forgot Password Page
    
    * Added Home Page Structure
    
    * Added Home Page Bg image
    
    * Added Pricing Page
    
    * Added Pricing Page
    
    * Added Keycloack
    
    * Added sturctures of Dashboard Pages
    
    * fix some bugs
    
    * bugs
    
    * Fixed bg color issue
    
    * Fixed header resuable issue
    
    * Added register functionality
    
    * Added register functionality
    
    * Added notification system using react portals
    
    * Added some alerts
    
    * Completed user registration
    
    * Added Login Functionality
    
    * Added Login Functionality
    
    * Added logout
    
    * changes
    
    * Added public and protected routes
    
    * Added authentication session checking
    
    * Feature/login page (#5)
    
    * Updated User service endpoints and configure them
    
    * Updated User service endpoints and configure them
    
    * Added reset password functionality
    
    * added single link page and links listing pages and api services
    
    * Added create and delete link operations
    
    * Added global links search
    
    * Added table links search
    
    * Added delete popup in the link list
    
    * Added avatar popup
    
    * Fixed bugs
    
    * Fixed links table styles
    
    * Added updated link
    
    * Make create new link unique
    
    * Changed alert styles
    
    * Added github actions
    
    * Added github actions
    
    * Feature/login page (#6)
    
    * Updated User service endpoints and configure them
    
    * Updated User service endpoints and configure them
    
    * Added reset password functionality
    
    * added single link page and links listing pages and api services
    
    * Added create and delete link operations
    
    * Added global links search
    
    * Added table links search
    
    * Added delete popup in the link list
    
    * Added avatar popup
    
    * Fixed bugs
    
    * Fixed links table styles
    
    * Added updated link
    
    * Make create new link unique
    
    * Changed alert styles
    
    * Added github actions
    
    * Added github actions
    
    * Added github actions
    
    * Added base url to env
    
    * Fixed issues
    
    * Fixed issues
    
    * Fixed issues
    
    * Fixed issues - 00
    
    * Added branch specific docker images creation
    
    * Added branch specific docker images creation
    
    * Dev (#9)
    
    * Fixed prod env issues - 01
    
    * Fixed prod env issues - 01
    
    * Fixed prod env issues - 02
    
    * Fixed prod env issues - 03

--- env.sh
+++ env.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -e
 
+# Export environment variables to make them available to envsubst
+export VITE_API_URL="${VITE_API_URL:-http://localhost:5000}"
+export MODE="${MODE:-production}"
+
 # Replace placeholders in env.template.js with actual environment variables
-envsubst < /usr/share/nginx/html/env.template.js > /usr/share/nginx/html/env.js
+# Explicitly list variables to substitute
+envsubst '$VITE_API_URL $MODE' < /usr/share/nginx/html/env.template.js > /usr/share/nginx/html/env.js
 
 # Start Nginx
 exec nginx -g 'daemon off;'
